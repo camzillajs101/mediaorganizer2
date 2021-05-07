fbed7948cdc9055b89fa35c49122a8b28136eb92
Camilo Mason <camilomason0@gmail.com>
2021-05-07 16:50:15 -0400

    stupid equals sign bug
    it's always that damn missing equals sign

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -78,7 +78,7 @@ class ImagesController < ApplicationController
     table = CSV.parse(uploaded_file.read)
 
     table.each do |row|
-      return if row[1] = "url"
+      return if row[0] == "id" # header row
       if Image.where(url: row[1]).empty?
         image = current_user.images.new(url: row[1], title: row[2], favorite: row[3], mediatype: row[6])
         image.save
